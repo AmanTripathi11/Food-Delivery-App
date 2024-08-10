7c79839cc08ac0255046a3bca4406833c74b66a8
Aman Tripathi <92243756+AmanTripathi11@users.noreply.github.com>
2024-08-10 13:48:56 +0530

    Update assets.js

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -3,7 +3,7 @@ import logo from './logo.png'
 import logo1 from './logo1.png'
 import logobg from './logobg.png'
 import header_img from './header_img.png'
-import search_icon from './search_icon.png'
+// import search_icon from './search_icon.png'
 import menu_1 from './menu_1.png'
 import menu_2 from './menu_2.png'
 import menu_3 from './menu_3.png'
@@ -48,7 +48,7 @@ import food_32 from './food_32.png'
 
 import add_icon_white from './add_icon_white.png'
 import add_icon_green from './add_icon_green.png'
-import remove_icon_red from './remove_icon_red.png'
+// import remove_icon_red from './remove_icon_red.png'
 import app_store from './app_store.png'
 import play_store from './play_store.png'
 import linkedin_icon from './linkedin_icon.png'
@@ -68,11 +68,11 @@ export const assets = {
     logobg,
     basket_icon,
     header_img,
-    search_icon,
+    // search_icon,
     rating_starts,
     add_icon_green,
     add_icon_white,
-    remove_icon_red,
+    // remove_icon_red,
     app_store,
     play_store,
     linkedin_icon,
